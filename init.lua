49d73254c556954d5645100f34b046290a1d6850
469714875@qq.com <469714875@qq.com>
2023-06-20 16:11:58 +0800

    add telescope.nvim

--- init.lua
+++ init.lua
@@ -56,6 +56,7 @@ require("lazy").setup(
 		opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help" } },	
 	},
 	{
+		cmd = "Telescope",
     		'nvim-telescope/telescope.nvim', tag = '0.1.1',
 		-- or                          , branch = '0.1.1',
       		dependencies = { 'nvim-lua/plenary.nvim' },
